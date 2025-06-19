cf97bbc777418d22fd9515374604e91262da1ddc
Shiblu <mohammad.shiblu@fau.de>
2025-06-19 14:36:20 +0200

    UI has been customized

--- app/screens/home_screen.py
+++ app/screens/home_screen.py
@@ -8,7 +8,7 @@ from kivy.metrics import sp, dp
 from kivy.core.image import Image as CoreImage
 from app.services.translator import translate_word
 from app.services.database import save_word
-
+from kivy.uix.boxlayout import BoxLayout
 
 class HomeScreen(Screen):
     def __init__(self, **kwargs):
@@ -21,83 +21,76 @@ class HomeScreen(Screen):
     def setup_ui(self):
         
         with self.canvas.before:
-            self.bg_image = Rectangle(
-                source='fig/background.png',
-                size=self.size,
-                pos=self.pos
-            )
+            Color(0.125, 0.698, 0.667, 1)
+            self.bg_rect = Rectangle(pos=self.pos, size=self.size)
         self.bind(size=self.update_bg, pos=self.update_bg)
         
+        layout = BoxLayout(orientation='vertical', spacing=dp(20), padding=dp(20))
+        # Title label
+        title =Label(
+            text='Vocab',
+            font_size=sp(42),
+            size_hint=(1, 0.2),
+            height=dp(30),
+            color=(0.2, 0.4, 0.4, 1),  
+            font_name = "fonts/Roboto/static/Roboto-Bold.ttf"
+        )
+        layout.add_widget(title)
+
+        # Buttons
+        buttons = [
+            ("Add Word", self.add_word),
+            ("Word List", self.word_list),
+            ("Practice", self.practice),
+            ("Quiz", self.quiz),
+        ]
+        for text, callback in buttons:
+            btn = Button(
+                text=text,
+                size_hint_y=None,
+                height=dp(60),
+                background_color=(1, 1, 1, 1),
+                color = (0.2, 0.4, 0.4, 1),
+                font_size= '18sp',
+                background_normal=''
+            )
+            with btn.canvas.before:
+                Color(1, 1, 1, 1)
+                btn.bg = RoundedRectangle(pos=btn.pos, size=btn.size, radius=[20])
+                btn.bind(pos=self.update_btn_bg(btn), size= self.update_btn_bg(btn))
+            btn.bind(on_press=callback)
+            layout.add_widget(btn)
+
+        self.add_widget(layout)
         
-
-        self.app_title = Label(
-            text = "Vocab",
-            font_size = sp(36),
-            color = (0.2, 0.4, 0.4, 1),
-            size_hint = None,
-            height = self.texture_size[1] + dp(10),
-        )
-
-        # Input and Output Widgets
-        self.input_box = TextInput(
-            hint_text='Enter a word',
-            font_size = 20,
-            size_hint=(0.6, 0.1),
-            pos_hint={'center_x': 0.5, 'center_y': 0.6},
-            background_color=(1, 1, 1, 0.8),
-            foreground_color=(0, 0, 0, 1),
-            multiline=False
-        )
-
-        self.output_label = Label(
-            text="Meaning will show here.",
-            font_size=18,
-            size_hint=(0.8, 0.2),
-            pos_hint={'center_x': 0.5, 'center_y': 0.4},
-            color=(0, 0, 0, 1),  
-        )
-        self.button = Button(
-            text='Search',
-            size_hint=(0.3, 0.1),
-            pos_hint={'center_x': 0.5, 'center_y': 0.25},
-            background_normal='',
-            background_color=(0.3, 0.6, 0.3, 1),
-            font_size=20
-        )
-        
-        self.button.bind(on_press=self.on_translate)
-
-        self.btn_word_list = Button(
-            text='View Saved Words',
-            size_hint=(0.3, 0.1),
-            pos_hint={'center_x': 0.5, 'center_y': 0.1},
-            background_normal='',
-            background_color=(0.3, 0.6, 0.3, 1),
-            font_size=20
-        )
-        self.btn_word_list.bind(on_press=self.go_to_word_list)
-
-        self.layout.add_widget(self.app_title)
-        self.layout.add_widget(self.input_box)
-        self.layout.add_widget(self.output_label)
-        self.layout.add_widget(self.button)
-        self.layout.add_widget(self.btn_word_list)
-        self.add_widget(self.layout)
 
     def update_bg(self, *args):
         self.bg_rect.pos = self.pos
         self.bg_rect.size = self.size
-    def on_translate(self, instance):
-        word = self.input_box.text.strip()
-        if not word:
-            self.output_label.text = "Please enter a word."
-            return
-        translation = translate_word(word)
-        if translation:
-            self.output_label.text = translation
-            save_word(word, translation)
-        else:
-            self.output_label.text = "Translation failed. Please try again."
 
-    def go_to_word_list(self, instance):
-        self.manager.current = 'word_list'
+    def update_btn_bg(self, btn):
+        def update(instance, value):
+            btn.bg.pos = instance.pos
+            btn.bg.size = instance.size
+        return update
+    
+    def add_word(self, instance): print("Add Word clicked")
+    def word_list(self, instance): print("Word List clicked")
+    def practice(self, instance): print("Practice clicked")
+    def quiz(self, instance): print("Quiz clicked")
+
+
+    # def on_translate(self, instance):
+    #     word = self.input_box.text.strip()
+    #     if not word:
+    #         self.output_label.text = "Please enter a word."
+    #         return
+    #     translation = translate_word(word)
+    #     if translation:
+    #         self.output_label.text = translation
+    #         save_word(word, translation)
+    #     else:
+    #         self.output_label.text = "Translation failed. Please try again."
+
+    # def go_to_word_list(self, instance):
+    #     self.manager.current = 'word_list'
