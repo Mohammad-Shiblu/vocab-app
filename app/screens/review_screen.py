a1a6b90ed3246cec3db32e661f50816252070f53
Shiblu <mohammad.shiblu@fau.de>
2025-06-27 03:25:49 +0200

    flash card play implemented

--- app/screens/review_screen.py
+++ app/screens/review_screen.py
@@ -1,7 +1,30 @@
 from kivy.uix.screenmanager import Screen
-from kivy.uix.label import Label
+from kivy.properties import StringProperty, BooleanProperty
+from app.services.database import get_all_words
 
 class ReviewScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.add_widget(Label(text="Review Screen - Coming Soon!"))
+    current_display = StringProperty("")
+    flipped = BooleanProperty(False)
+
+    def on_enter(self):
+        self.words = get_all_words()
+        self.index = 0
+        self.flipped = False
+        self.current_display = self.words[self.index][0] if self.words else "No words available"
+
+    def flip_card(self):
+        if not self.words:
+            return
+        word, meaning = self.words[self.index]
+        self.flipped = not self.flipped
+        self.current_display = meaning if self.flipped else word
+
+    def next_card(self):
+        if not self.words:
+            return
+        self.index = (self.index + 1) % len(self.words)
+        self.flipped = False
+        self.current_display = self.words[self.index][0]
+
+    def go_back(self):
+        self.manager.current = "home"
